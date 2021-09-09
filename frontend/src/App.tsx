153098d775f3c3b050245a93d7ea04d3189d8c1a
GeiBatista <geibatista@hotmail.com>
2021-09-08 23:27:44 -0300

    Gráficos com dados estáticos

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,8 @@
 import NavBar from "components/NavBar";
 import Footer from "components/Footer";
 import DataTable from "components/DataTable";
+import BarChart from "components/BarChart";
+import DonutChart from "components/DonutChart";
 
 
 function App() {
@@ -8,9 +10,25 @@ function App() {
     <>
       <NavBar />
     <div className="container">
-      <h1 className="text-primary">Olá mundo!</h1>
-    </div>  
+      <h1 className="text-primary py-3">Dashboard de Vendas</h1>
+    
+    <div className="row px-3">
+    <div className="col-sm-6">
+      <h5 className="text-center text-secondary" >Percentual de Sucesso (%)</h5>
+      <BarChart />
+    </div>    
+    <div className="col-sm-6">
+      <h5 className="text-center text-secondary" >Vendas Totais</h5>
+      <DonutChart />
+    </div>
+    </div>
+
+    <div className="py-3">
+    <h2 className="text-primary">Vendas Totais</h2>
+    </div>
+
     <DataTable />
+    </div>  
     <Footer /> 
     </> 
   );
