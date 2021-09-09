91520d845b8be277a2fdc79138b9ba6861558f4d
GeiBatista <geibatista@hotmail.com>
2021-09-08 22:42:17 -0300

    Componentes Básico estáticos

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,18 @@
+import NavBar from "components/NavBar";
+import Footer from "components/Footer";
+import DataTable from "components/DataTable";
+
+
 function App() {
   return (
-    <div>
+    <>
+      <NavBar />
+    <div className="container">
       <h1 className="text-primary">Olá mundo!</h1>
-    </div>    
+    </div>  
+    <DataTable />
+    <Footer /> 
+    </> 
   );
 }
 
