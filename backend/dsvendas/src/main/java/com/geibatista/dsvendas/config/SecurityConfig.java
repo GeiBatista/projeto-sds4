2ecb2875a866671ee5a59c34a7c83bba3e6641bc
GeiBatista <geibatista@hotmail.com>
2021-09-09 21:14:49 -0300

    Security config

---
+++ backend/dsvendas/src/main/java/com/geibatista/dsvendas/config/SecurityConfig.java
@@ -0,0 +1,53 @@
+package com.geibatista.dsvendas.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Autowired
+	private Environment env;
+
+//	Configuraçao para liberar o aplicativo de gerenciamento do banco de dados H2
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		if (Arrays.asList(env.getActiveProfiles()).contains("test")) {
+			http.headers().frameOptions().disable();
+		}
+
+		http.cors().and().csrf().disable();
+		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+		http.authorizeRequests().anyRequest().permitAll();
+	}
+
+	/*
+	 Objeto instanciado para definir as configurações de CORS, ou seja o acesso foi liberado, 
+	 o front-end vai poder acessar o front-end
+	 O CORS (Cross-origin Resource Sharing) é um mecanismo utilizado pelos
+	 navegadores para compartilhar recursos entre diferentes origens. O CORS é uma
+	 especificação do W3C e faz uso de headers do HTTP para informar aos
+	 navegadores se determinado recurso pode ser ou não acessado.
+	 */
+
+	@Bean
+	CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration configuration = new CorsConfiguration().applyPermitDefaultValues();
+		configuration.setAllowedMethods(Arrays.asList("POST", "GET", "PUT", "DELETE", "OPTIONS"));
+		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", configuration);
+		return source;
+	}
+}
